9acee0c1e9cb5266a09ac7ca3bf18e46619d697e
chenlin <1243531712@qq.com>
2023-09-06 16:57:52 +0800

    transtion

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in" appear>
+    <component :is="Component" />
+  </transition>
+</router-view>
 </template>
+
+<style scoped>
+.fade-enter-active {
+  transition: all 0.3s;
+}
+
+/* 可能为enter失效，拆分为 enter-from和enter-to */
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
